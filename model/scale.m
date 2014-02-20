db7c162fe9502995bc2edd89654a2a32d96ee1dd
Simon Maurer <moirelein@gmail.com>
2014-02-20 23:24:14 +0100

    fix comentary

--- model/scale.m
+++ model/scale.m
@@ -1,6 +1,6 @@
 function [c] = scale (val)
     % scale values
     % alpha: input value to scale
-    % result: log-likelihood of a sequence
+    % result: scaled values
     c = 1 ./ sum(val); % eq. 6.18
 end;
