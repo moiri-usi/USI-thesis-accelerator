975892ce7380bb8673dcca36e10dee975d46d048
Simon Maurer <moirelein@gmail.com>
2014-06-05 21:47:09 +0200

    better testcase. new error: shift reg elements need to be crossed over

--- nexys4/test/forward_s.tcl
+++ nexys4/test/forward_s.tcl
@@ -1,8 +1,108 @@
 isim force add clk 0 -time 0 -value 1 -time 5ns -repeat 10ns
 isim force add reset_n 0
-isim force add b_in 87244
-isim force add tp_in 60555
-isim force add pi_in 6301857
 run 100ns
 isim force add reset_n 1
-run 2000ns
+run 20ns
+
+isim force add tp_in 191627
+run 10ns
+isim force add tp_in 3386
+run 10ns
+isim force add tp_in 67129
+run 10ns
+isim force add b_in 87244
+run 10ns
+isim force add pi_in 6301857
+run 50ns
+
+isim force add tp_in 41300
+run 10ns
+isim force add tp_in 119888
+run 10ns
+isim force add tp_in 100954
+run 10ns
+isim force add b_in 12928
+run 10ns
+isim force add pi_in 11998393
+run 50ns
+
+isim force add tp_in 34
+run 10ns
+isim force add tp_in 7636
+run 10ns
+isim force add tp_in 254473
+run 10ns
+isim force add b_in 161970
+run 10ns
+isim force add pi_in 15254180
+run 50ns
+
+
+
+isim force add tp_in 191627
+run 10ns
+isim force add tp_in 3386
+run 10ns
+isim force add tp_in 67129
+run 10ns
+isim force add b_in 87244
+run 10ns
+isim force add pi_in 6301857
+run 50ns
+
+isim force add tp_in 41300
+run 10ns
+isim force add tp_in 119888
+run 10ns
+isim force add tp_in 100954
+run 10ns
+isim force add b_in 12928
+run 10ns
+isim force add pi_in 11998393
+run 50ns
+
+isim force add tp_in 34
+run 10ns
+isim force add tp_in 7636
+run 10ns
+isim force add tp_in 254473
+run 10ns
+isim force add b_in 161970
+run 10ns
+isim force add pi_in 15254180
+run 50ns
+
+
+
+isim force add tp_in 191627
+run 10ns
+isim force add tp_in 3386
+run 10ns
+isim force add tp_in 67129
+run 10ns
+isim force add b_in 87244
+run 10ns
+isim force add pi_in 6301857
+run 50ns
+
+isim force add tp_in 41300
+run 10ns
+isim force add tp_in 119888
+run 10ns
+isim force add tp_in 100954
+run 10ns
+isim force add b_in 12928
+run 10ns
+isim force add pi_in 11998393
+run 50ns
+
+isim force add tp_in 34
+run 10ns
+isim force add tp_in 7636
+run 10ns
+isim force add tp_in 254473
+run 10ns
+isim force add b_in 161970
+run 10ns
+isim force add pi_in 15254180
+run 200ns
