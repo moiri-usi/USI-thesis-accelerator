e3abcd9b80579e6375af3fc412e0c8cc1e803eef
Simon Maurer <moirelein@gmail.com>
2014-06-13 12:00:50 +0200

    sequential testdata

--- nexys4/test/forward_s.tcl
+++ nexys4/test/forward_s.tcl
@@ -4,142 +4,94 @@ isim force add tp_we 0
 isim force add pi_we 0
 isim force add b_we 0
 isim force add data_ready 0
-run 20ns
+run 10ns
 isim force add reset_n 1
-run 10ns
+run 30ns
 isim force add pi_we 1
-isim force add pi_in 6301857
-run 10ns
-isim force add pi_in 11998393
-run 10ns
-isim force add pi_in 15254180
-run 10ns
-isim force add pi_we 0
 isim force add tp_we 1
+isim force add b_we 1
+isim force add pi_in 6301857
+isim force add b_in 87244
 isim force add tp_in 191627
 run 10ns
 isim force add data_ready 1
+isim force add pi_in 11998393
+isim force add b_in 12928
 isim force add tp_in 3386
 run 10ns
+isim force add pi_in 15254180
+isim force add b_in 161970
 isim force add tp_in 67129
-isim force add b_we 1
-isim force add b_in 87244
 run 10ns
-isim force add tp_we 0
+isim force add pi_we 0
 isim force add b_we 0
-run 10ns
-run 50ns
-
-isim force add tp_we 1
 isim force add tp_in 41300
 run 10ns
 isim force add tp_in 119888
 run 10ns
 isim force add tp_in 100954
-isim force add b_we 1
-isim force add b_in 12928
 run 10ns
-isim force add tp_we 0
-isim force add b_we 0
-run 10ns
-run 50ns
-
-isim force add tp_we 1
 isim force add tp_in 34
 run 10ns
 isim force add tp_in 7636
 run 10ns
 isim force add tp_in 254473
-isim force add b_we 1
-isim force add b_in 161970
 run 10ns
 isim force add tp_we 0
-isim force add b_we 0
-run 10ns
-run 60ns
+run 190ns
 
 
 isim force add tp_we 1
+isim force add b_we 1
 isim force add tp_in 191627
-run 10ns
-isim force add tp_in 3386
-run 10ns
-isim force add tp_in 67129
-isim force add b_we 1
 isim force add b_in 87244
 run 10ns
-isim force add tp_we 0
-isim force add b_we 0
+isim force add tp_in 3386
+isim force add b_in 12928
 run 10ns
-run 50ns
-
-isim force add tp_we 1
+isim force add tp_in 67129
+isim force add b_in 161970
+run 10ns
+isim force add b_we 0
 isim force add tp_in 41300
 run 10ns
 isim force add tp_in 119888
 run 10ns
 isim force add tp_in 100954
-isim force add b_we 1
-isim force add b_in 12928
 run 10ns
-isim force add tp_we 0
-isim force add b_we 0
-run 10ns
-run 50ns
-
-isim force add tp_we 1
 isim force add tp_in 34
 run 10ns
 isim force add tp_in 7636
 run 10ns
 isim force add tp_in 254473
-isim force add b_we 1
-isim force add b_in 161970
 run 10ns
 isim force add tp_we 0
-isim force add b_we 0
-run 10ns
-run 60ns
+run 190ns
 
 
 isim force add tp_we 1
+isim force add b_we 1
 isim force add tp_in 191627
-run 10ns
-isim force add tp_in 3386
-run 10ns
-isim force add tp_in 67129
-isim force add b_we 1
 isim force add b_in 87244
 run 10ns
-isim force add tp_we 0
-isim force add b_we 0
+isim force add tp_in 3386
+isim force add b_in 12928
 run 10ns
-run 50ns
-
-isim force add tp_we 1
+isim force add tp_in 67129
+isim force add b_in 161970
+run 10ns
+isim force add b_we 0
 isim force add tp_in 41300
 run 10ns
 isim force add tp_in 119888
 run 10ns
 isim force add tp_in 100954
-isim force add b_we 1
-isim force add b_in 12928
 run 10ns
-isim force add tp_we 0
-isim force add b_we 0
-run 10ns
-run 50ns
-
-isim force add tp_we 1
 isim force add tp_in 34
 run 10ns
 isim force add tp_in 7636
 run 10ns
 isim force add tp_in 254473
-isim force add b_we 1
-isim force add b_in 161970
 run 10ns
 isim force add tp_we 0
-isim force add b_we 0
-run 10ns
-run 100ns
+run 230ns
