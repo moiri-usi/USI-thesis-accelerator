967f037335207302bd58925949ea043fe58be69f
Simon Maurer <moirelein@gmail.com>
2014-06-25 13:35:39 +0200

    optimized matrix implementation of scaled basic algorithm

---
+++ model/test_scaling_p.m
@@ -0,0 +1,26 @@
+% initialisation of test values
+% ==============================================================================
+OP1_WIDTH = 25;
+OP2_WIDTH = 18;
+N = 100; % number of states
+M = 1000; % number of observation symbols
+L = 200; % sequence lengt
+seq_e = randint(1, L, [1 M]); % example sequence
+alphabet = [1:M]; % alphabet (list of observation symbols)
+% PI: initial state probability vector. size N
+PI = abs(rand(N, 1));
+PI = PI./sum(PI);
+% B: matrix of emission probabilities
+B = abs(rand(N, M)); % size N, M
+B = B./repmat(sum(B, 2), 1, M);
+% P: matrix of limiting tansmission probabilities
+TP = abs(rand(N, N)); % size N, N
+TP = TP./repmat(sum(TP, 2), 1, N);
+
+% sequence detection
+% ==============================================================================
+tic;
+%for i=1:1000,
+    Ps = forward_p_scaling(N, L, PI, B, TP, seq_e)
+%end
+toc;
