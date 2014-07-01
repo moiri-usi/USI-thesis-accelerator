3a6cf6fb867ff648e2ecb69f82dc5586e4c166d7
Simon Maurer <moirelein@gmail.com>
2014-07-01 23:30:56 +0200

    add p (bug fix)

--- model/compute_tp.m
+++ model/compute_tp.m
@@ -4,23 +4,24 @@
 % @param N:         number of states
 % @param dk:        delay of k-th observation symbol
 % @param cdf_param: parameters for the cdf
-% @return v:        extended transition probabilities
+% @param P:         transmission probabilities
+% @return V:        extended transition probabilities
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [v] = compute_tp(N, dk, cdf_param)
+function [V] = compute_tp(N, dk, cdf_param, P)
     % compute all elements of v
     for i=1:N,
         for j=1:N,
-            v(i, j) = normcdf(dk, cdf_param.mu(i, j), cdf_param.sigma(i, j));
+            V(i, j) = P(i, j)*normcdf(dk, cdf_param.mu(i, j), cdf_param.sigma(i, j));
         end
     end
     % correct diagonal elemnts of v
     for i=1:N,
         for j=1:N,
-            v_sum(i) += v(i, j);
+            V_sum(i) += V(i, j);
         end
     end
     for i=1:N,
-        v_sum(i) -= v(i, i);
-        v(i, i) = 1 - v_sum(i);
+        V_sum(i) -= V(i, i);
+        V(i, i) = 1 - V_sum(i);
     end
 end
